a631f1d4180ab0e624b6695f0ae6f751eeaa34ab
soumya-fogo <soumya@fogosol.com>
2020-04-18 18:21:01 +0530

    added new project

---
+++ src/main/java/com/logicbig/example/HelloServiceMain.java
@@ -0,0 +1,13 @@
+package com.logicbig.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HelloServiceMain{
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceMain.class, args);
+	}
+}
